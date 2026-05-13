b5f15e3e0f5f4a195454ea4e4420f6be1173738b
StefanDeimel <stefan.deimel@aon.at>
2026-05-13 14:13:26 +0200

    change reflow configuration

--- core/gdrn_modeling/demo/predictor_gdrn.py
+++ core/gdrn_modeling/demo/predictor_gdrn.py
@@ -38,6 +38,7 @@ from setproctitle import setproctitle
 from mmcv import Config
 from scipy.spatial.transform import Rotation
 
+import copy
 import rospy
 import yaml
 from core.gdrn_modeling.models import (
@@ -50,17 +51,18 @@ from core.gdrn_modeling.models import (
     GDRN_Rho_flow,
 )  # noqa
 
-def align_sympose_with_vector(vector, pose, align_axis, rot_axis):
+def align_sympose_with_vector(vector, pose, align_axis, rot_axis): 
     vector /= np.linalg.norm(vector)
-    proj_vector_on_rot_axis = np.dot(vector, pose[:3, rot_axis]) *pose[:3, rot_axis]
-    proj_vector_rot_plane = vector - proj_vector_on_rot_axis
-    proj_vector_rot_plane /= np.linalg.norm(proj_vector_rot_plane)
-    alpha = math.acos(np.dot(pose[:3, align_axis], proj_vector_rot_plane))
+    proj_vector_on_rot_axis = np.dot(vector, pose[:3, rot_axis]) *pose[:3, rot_axis] 
+    proj_vector_rot_plane = vector - proj_vector_on_rot_axis 
+    proj_vector_rot_plane /= np.linalg.norm(proj_vector_rot_plane) 
+    alpha = math.acos(np.dot(pose[:3, align_axis], proj_vector_rot_plane)) 
     if np.dot(np.cross(proj_vector_rot_plane, pose[:3, align_axis]), pose[:3, rot_axis]) > 0:
         alpha *= -1
     euler_angles = [0., 0., 0.]
     euler_angles[rot_axis] = alpha
     new_rot = np.dot(pose[:3, :3], tf3d.euler.euler2mat(*euler_angles, 'sxyz'))
+    # new_rot = np.matmul(pose[:3, :3], tf3d.euler.euler2mat(*euler_angles, 'sxyz'))
 
     return new_rot
 
@@ -215,7 +217,6 @@ class GdrnPredictor():
                 )
             data_dict["cur_res"].append(cur_res)
 
-
         if not reflow and self.cfg.TEST.USE_DEPTH_REFINE:
            self.process_depth_refine(data_dict, out_dict, renderer_request_queue, renderer_result_queue)
 
@@ -225,18 +226,15 @@ class GdrnPredictor():
             pose[:3, :3] = res['R']
             pose[:3, 3] = res['t']
 
-            if len(plane_normal) != 0:
-                tmp_pose = np.eye(4)
-                tmp_pose[:3, :3] = res['R']
-                tmp_pose[:3, 3] = res['t']
-
-                if reflow:
-                    z_vec = np.array([0.,0.,-1.])
-                    proj_vector_on_plane_normal = np.dot(z_vec, plane_normal) * plane_normal
-                    x_plane_vec = z_vec - proj_vector_on_plane_normal
-                    pose[:3, :3] = align_sympose_with_vector(x_plane_vec, tmp_pose, 0, 2)
-
+            if reflow and len(plane_normal) != 0:
+                tmp_pose = copy.deepcopy(pose)
+                z_vec = np.array([0.,0.,-1.])
+                proj_vector_on_plane_normal = np.dot(z_vec, plane_normal) * plane_normal
+                x_plane_vec = z_vec - proj_vector_on_plane_normal # x_plane_vec is no normal to proj_vector_on_plane_normal, so on the plane
+                pose[:3, :3] = align_sympose_with_vector(x_plane_vec, tmp_pose, 0, 2)
+                
             poses[self.objs.get(res['obj_id'])] = pose
+
 
         return poses
 
@@ -698,52 +696,54 @@ class GdrnPredictor():
         # cfg.freeze()
         return cfg
 
-    # def gdrn_visualization(self, batch, out_dict, image):
-    #     vis_dict = {}
+    def gdrn_visualization(self, batch, out_dict, image):
+        vis_dict = {}
 
-    #     # for crop and resize
-    #     bs = batch["roi_cls"].shape[0]
-    #     #print(bs)
-    #     tensor_kwargs = {"dtype": torch.float32, "device": "cuda"}
-    #     rois_xy0 = batch["roi_center"] - batch["scale"].view(bs, -1) / 2  # bx2
-    #     rois_xy1 = batch["roi_center"] + batch["scale"].view(bs, -1) / 2  # bx2
-    #     batch["inst_rois"] = torch.cat([torch.arange(bs, **tensor_kwargs).view(-1, 1), rois_xy0, rois_xy1], dim=1)
+        # for crop and resize
+        bs = batch["roi_cls"].shape[0]
+        # print(f"=== [HELP] batch[roi_cls].shape[0]={bs}, batch[roi_cls]={batch['roi_cls']}")
+        tensor_kwargs = {"dtype": torch.float32, "device": "cuda"}
+        rois_xy0 = batch["roi_center"] - batch["scale"].view(bs, -1) / 2  # bx2
+        rois_xy1 = batch["roi_center"] + batch["scale"].view(bs, -1) / 2  # bx2
+        batch["inst_rois"] = torch.cat([torch.arange(bs, **tensor_kwargs).view(-1, 1), rois_xy0, rois_xy1], dim=1)
 
-    #     im_H = int(batch["im_H"][0])
-    #     im_W = int(batch["im_W"][0])
-    #     if "full_mask" in out_dict:
-    #         raw_full_masks = out_dict["full_mask"]
-    #         full_mask_probs = get_out_mask(self.cfg, raw_full_masks)
-    #         full_masks_in_im = paste_masks_in_image(
-    #             full_mask_probs[:, 0, :, :],
-    #             batch["inst_rois"][:, 1:5],
-    #             image_shape=(im_H, im_W),
-    #             threshold=0.5,
-    #         )
-    #         full_masks_np = full_masks_in_im.detach().to(torch.uint8).cpu().numpy()
+        im_H = int(batch["im_H"][0])
+        im_W = int(batch["im_W"][0])
+        if "full_mask" in out_dict:
+            raw_full_masks = out_dict["full_mask"]
+            full_mask_probs = get_out_mask(self.cfg, raw_full_masks)
+            full_masks_in_im = paste_masks_in_image(
+                full_mask_probs[:, 0, :, :],
+                batch["inst_rois"][:, 1:5],
+                image_shape=(im_H, im_W),
+                threshold=0.5,
+            )
+            full_masks_np = full_masks_in_im.detach().to(torch.uint8).cpu().numpy()
 
-    #         img_vis_full_mask = vis_image_mask_bbox_cv2(
-    #             image,
-    #             [full_masks_np[i] for i in range(bs)],
-    #             [batch["bbox_est"][i].detach().cpu().numpy() for i in range(bs)],
-    #             labels=self.cls_names,
-    #         )
+            img_vis_full_mask = vis_image_mask_bbox_cv2(
+                image,
+                [full_masks_np[i] for i in range(bs)],
+                [batch["bbox_est"][i].detach().cpu().numpy() for i in range(bs)],
+                labels=self.cls_names,
+            )
 
-    #         vis_dict[f"im_det_and_mask_full"] = img_vis_full_mask[:, :, ::-1]
+            # print(f"=== [HELP] im_det_and_mask_full")
+            vis_dict[f"im_det_and_mask_full"] = img_vis_full_mask[:, :, ::-1]
 
-    #     for i in range(bs):
-    #         R = batch["cur_res"][i]["R"]
-    #         t = batch["cur_res"][i]["t"]
-    #         # pose_est = np.hstack([R, t.reshape(3, 1)])
-    #         proj_pts_est = misc.project_pts(self.obj_models[i+1]["pts"], self.cam, R, t)
-    #         mask_pose_est = misc.points2d_to_mask(proj_pts_est, im_H, im_W)
-    #         image_mask_pose_est = vis_image_mask_cv2(image, mask_pose_est, color="yellow" if i == 0 else "blue")
-    #         image_mask_pose_est = vis_image_bboxes_cv2(
-    #             image_mask_pose_est,
-    #             [batch["bbox_est"][i].detach().cpu().numpy()],
-    #             labels=[self.cls_names[i]]
-    #         )
-    #         vis_dict[f"im_{i}_mask_pose_est"] = image_mask_pose_est[:, :, ::-1]
-    #     show_ims = np.hstack([cv2.cvtColor(_v, cv2.COLOR_BGR2RGB) for _k, _v in vis_dict.items()])
-    #     cv2.imshow('result', show_ims)
-    #     cv2.waitKey(1)
+        for i in range(bs):
+            R = batch["cur_res"][i]["R"]
+            t = batch["cur_res"][i]["t"]
+            # pose_est = np.hstack([R, t.reshape(3, 1)])
+            proj_pts_est = misc.project_pts(self.obj_models[i+1]["pts"], self.cam, R, t)
+            mask_pose_est = misc.points2d_to_mask(proj_pts_est, im_H, im_W)
+            image_mask_pose_est = vis_image_mask_cv2(image, mask_pose_est, color="yellow" if i == 0 else "blue")
+            image_mask_pose_est = vis_image_bboxes_cv2(
+                image_mask_pose_est,
+                [batch["bbox_est"][i].detach().cpu().numpy()],
+                labels=[self.cls_names[i]]
+            )
+            # print(f"=== [HELP] im_{i}_mask_pose_est")
+            vis_dict[f"im_{i}_mask_pose_est"] = image_mask_pose_est[:, :, ::-1]
+        show_ims = np.hstack([cv2.cvtColor(_v, cv2.COLOR_BGR2RGB) for _k, _v in vis_dict.items()])
+        cv2.imshow('result', show_ims)
+        cv2.waitKey(1)
